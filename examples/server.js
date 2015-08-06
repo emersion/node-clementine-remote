dea3a38798203bc382b8061f45457004cde0658a
emersion <contact@emersion.fr>
2015-08-06 17:46:51 +0200

    Adds multiple playlists support

--- examples/server.js
+++ examples/server.js
@@ -1,12 +1,15 @@
 var ClementineServer = require('..').Server;
 
 var server = ClementineServer({
+	host: '0.0.0.0',
 	port: 5500,
-	auth_code: 42
+	//auth_code: 42
 });
 
 server.on('playpause', function () {
 	console.log('playpause');
 });
 
-server.listen();
+server.listen(function () {
+	server.mdns();
+});
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 exports.Client = require('./lib/client');
 exports.Server = require('./lib/server');
+
+exports.Playlist = require('./lib/playlist');
